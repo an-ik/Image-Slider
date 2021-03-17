9ebca90c915431a61d18aad52089f1061a6c15fe
Andy <adominik@massmutual.com>
2021-03-16 20:32:46 -0400

    Fixing touch

--- js/view-master.js
+++ js/view-master.js
@@ -105,30 +105,28 @@ document.addEventListener('keyup', function(e) {
           }
 })
 
-//Touch support
+// //Touch support
 let startX = null;
 let viewPortWidth = null;
 var initialSetLeftPosition = null;
-var clicked = false;
+
 
 function getStartX(e) {
           e.preventDefault();
-          imgReel.classList.remove('smooth');
           initialSetLeftPosition = setLeftPosition;
           viewPortWidth = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
-          startX = Math.round((e.clientX / viewPortWidth) * 100);
-          clicked = true;
+          startX = Math.round((e.changedTouches[0].clientX / viewPortWidth) * 100);
 }
 
 function getNewXandMoveSlider(e) {
           e.preventDefault();
-          var newX = Math.round((e.clientX / viewPortWidth) * 100);
-          if (newX < startX && newX > 0 && setLeftPosition < ((imgArray.length-1) * 100) && clicked) {
+          var newX = Math.round((e.changedTouches[0].clientX / viewPortWidth) * 100);
+          if (newX < startX && newX > 0 && setLeftPosition < ((imgArray.length-1) * 100)) {
                     var dX = startX - newX;
                     setLeftPosition = initialSetLeftPosition + dX;
                     slideimgReel();
           }
-          if (newX > startX && newX < 100 && setLeftPosition > 0 && clicked) {
+          if (newX > startX && newX < 100 && setLeftPosition > 0) {
                     var dX = newX - startX;
                     setLeftPosition = initialSetLeftPosition - dX;
                     slideimgReel();
@@ -137,8 +135,8 @@ function getNewXandMoveSlider(e) {
 
 function getLastXandPinSlider(e) {
           e.preventDefault();
-          lastX = Math.round((e.clientX / viewPortWidth) * 100);
-          imgReel.classList.add('smooth');
+          lastX = Math.round((e.changedTouches[0].clientX / viewPortWidth) * 100);
+          console.log(startX, lastX);
           if (startX < lastX && setLeftPosition > 0) {
                     setLeftPosition = (Math.floor(setLeftPosition / 100)) * 100;
                     slideimgReel();
@@ -146,19 +144,15 @@ function getLastXandPinSlider(e) {
                     updatePagination();
                     addRemoveNextPreviousButtons();
           }
-          if (startX > lastX && setLeftPosition < ((imgArray.length - 1) * 100)) {
+          if (startX > lastX && setLeftPosition < ((imgArray.length-1) * 100)) {
                     setLeftPosition = (Math.ceil(setLeftPosition / 100)) * 100;
                     slideimgReel();
                     imgArrayIndex++;
                     updatePagination();
                     addRemoveNextPreviousButtons();
           }
-          clicked = false;
 }
 
-imgReel.addEventListener('touchstart', getStartX, false);
-imgReel.addEventListener('mousedown', getStartX, false);
-imgReel.addEventListener('touchmove', getNewXandMoveSlider, false);
-imgReel.addEventListener('mousemove', getNewXandMoveSlider, false); 
-imgReel.addEventListener('touchend', getLastXandPinSlider, false);
-imgReel.addEventListener('mouseup', getLastXandPinSlider, false);
+imgReel.addEventListener('touchstart', getStartX, false)
+imgReel.addEventListener('touchmove', getNewXandMoveSlider, false)
+imgReel.addEventListener('touchend', getLastXandPinSlider, false)

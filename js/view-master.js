bd48253dba5e4e4beef59dd96db67d9e322800f0
Andy <adominik@massmutual.com>
2021-03-17 10:21:29 -0400

    add JS documentation

--- js/view-master.js
+++ js/view-master.js
@@ -13,7 +13,7 @@ var       viewMaster          = document.querySelector('.view-master'),
           imgArrayIndex       = 0;
 
 //Functions
-const createNewImageListNoATag = (function () {                                                     //Clone images from gallery
+const createNewImageListNoATag = (function () {                                                     //Clone images from gallery and builds imgReel
           let ul = document.createElement("ul");                                                    //Create ul
           for (var i = 0; i < imgArray.length; i++) {                                               
                     let li = document.createElement("li");                                          //Create li
@@ -32,12 +32,13 @@ findClickedImageIndex = (e) => {
 }
 
 toggleViewMaster = () => {
-          viewMasterModal.classList.toggle('modal-is-displayed');                                   //add class to show view master modal
-          viewMaster.classList.toggle('show-view-master');                                          //add class to show view master
-          document.querySelector('body').classList.toggle('stopScroll');                            //add class to stop background scroll
+          viewMasterModal.classList.toggle('display-modal');                                        //add class to show view master modal
+          viewMaster.classList.toggle('display-view-master');                                       //add class to show view master
+          document.querySelector('body').classList.toggle('stop-scroll');                           //add class to stop background scroll
 }
 
-function slideimgReel() {
+slideimgReel = () => {
+          imgReel.classList.add('slide-effect');
           imgReel.style.transform = 'translate(-' + setLeftPosition + '%)';                         //slide imgReel based on the value of setLeftPosition
 }
 
@@ -64,17 +65,17 @@ displayPreviousImage = () => {
 }
 
 addRemoveNextPreviousButtons = () => {
-          if (setLeftPosition == ((imgArray.length - 1) * 100) || setLeftPosition == 0) {           //add class if last image is displayed
-                    nextButton.classList.add('d-none');
+          if (setLeftPosition == ((imgArray.length - 1) * 100)) {                                   //add class if last image is displayed
+                    nextButton.classList.add('disable-button');
           }
           if (setLeftPosition < ((imgArray.length - 1) * 100)) {                                    //remove class if last image is not displayed 
-                    nextButton.classList.remove('d-none');
+                    nextButton.classList.remove('disable-button');
           }
           if (setLeftPosition == 0) {
-                    previousButton.classList.add('d-none');                                         //add class if last image is displayed
+                    previousButton.classList.add('disable-button');                                 //add class if last image is displayed
           }
           if (setLeftPosition > 0) {
-                    previousButton.classList.remove('d-none');                                      //remove class if last image is not displayed
+                    previousButton.classList.remove('disable-button');                              //remove class if last image is not displayed
           }
           else return;
 }
@@ -84,10 +85,10 @@ updatePagination = () => {
 }
 
 //Click Event listeners
-nextButton.addEventListener('click', displayNextImage)
-previousButton.addEventListener('click', displayPreviousImage)
-closeButton.addEventListener('click', toggleViewMaster)
-galleryUl.addEventListener('click', function (e) {
+nextButton.addEventListener('click', displayNextImage)                                              //display next image
+previousButton.addEventListener('click', displayPreviousImage)                                      //display previous image
+closeButton.addEventListener('click', toggleViewMaster)                                             //close view master
+galleryUl.addEventListener('click', function (e) {                                                  //when a gallery image is clicked
           findClickedImageIndex(e);
           toggleViewMaster();
           slideimgReel();
@@ -96,57 +97,55 @@ galleryUl.addEventListener('click', function (e) {
 })
 //Key Event listeners
 document.addEventListener('keyup', function(e) {   
-          if (e.keyCode == 39) {displayNextImage();}
-          if (e.keyCode == 37) {displayPreviousImage();}
-          if (e.keyCode == 27) {
-                    if (viewMasterModal.classList == ('view-master-modal modal-is-displayed')) {
+          if (e.keyCode == 39) {displayNextImage();}                                                //right arrow display next image
+          if (e.keyCode == 37) {displayPreviousImage();}                                            //left arrow display previous image
+          if (e.keyCode == 27) {                                                                    //esc close view master
+                    if (viewMasterModal.classList == ('view-master-modal display-modal')) {         //prevent toggle when view master is not displayed
                               toggleViewMaster();
                     }
           }
 })
 
-// //Touch support
-let startX = null;
+//Touch support
+let startX = null;                                          
 let viewPortWidth = null;
 var initialSetLeftPosition = null;
-
 
 function getStartX(e) {
           e.preventDefault();
           initialSetLeftPosition = setLeftPosition;
-          viewPortWidth = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
-          startX = Math.round((e.changedTouches[0].clientX / viewPortWidth) * 100);
+          viewPortWidth = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);        //get viewport width
+          startX = Math.round((e.changedTouches[0].clientX / viewPortWidth) * 100);                           //get x position from initial touch and convert to percent of screen (1-100)
 }
 
 function getNewXandMoveSlider(e) {
           e.preventDefault();
-          var newX = Math.round((e.changedTouches[0].clientX / viewPortWidth) * 100);
-          if (newX < startX && newX > 0 && setLeftPosition < ((imgArray.length-1) * 100)) {
-                    var dX = startX - newX;
-                    setLeftPosition = initialSetLeftPosition + dX;
-                    slideimgReel();
+          var newX = Math.round((e.changedTouches[0].clientX / viewPortWidth) * 100);                         //get x position from moving touch and convert to percent of screen (1-100)
+          if (newX < startX && newX > 0 && setLeftPosition < ((imgArray.length-1) * 100)) {                   //if left swipe direction, in viewport, and not the last slide
+                    var dX = startX - newX;                                                                   //calculate difference
+                    setLeftPosition = initialSetLeftPosition + dX;                                            //calculate new setLeftPosition
+                    slideimgReel();                                                                           //call function
           }
-          if (newX > startX && newX < 100 && setLeftPosition > 0) {
-                    var dX = newX - startX;
-                    setLeftPosition = initialSetLeftPosition - dX;
-                    slideimgReel();
+          if (newX > startX && newX < 100 && setLeftPosition > 0) {                                           //if right swipe direction, in viewport, and not first slide
+                    var dX = newX - startX;                                                                   //calculate difference
+                    setLeftPosition = initialSetLeftPosition - dX;                                            //calculate new setLeftPosition
+                    slideimgReel();                                                                           //call function
           }
 }
 
 function getLastXandPinSlider(e) {
           e.preventDefault();
-          lastX = Math.round((e.changedTouches[0].clientX / viewPortWidth) * 100);
-          console.log(startX, lastX);
-          if (startX < lastX && setLeftPosition > 0) {
-                    setLeftPosition = (Math.floor(setLeftPosition / 100)) * 100;
+          lastX = Math.round((e.changedTouches[0].clientX / viewPortWidth) * 100);                            //get x position of last touch and convert to percent of screen (1-100)
+          if (startX < lastX && setLeftPosition > 0) {                                                        //if right swipe direction and not first slide 
+                    setLeftPosition = (Math.floor(setLeftPosition / 100)) * 100;                              //floor setLeftPosition       
                     slideimgReel();
                     imgArrayIndex--;
                     updatePagination();
                     addRemoveNextPreviousButtons();
           }
-          if (startX > lastX && setLeftPosition < ((imgArray.length-1) * 100)) {
-                    setLeftPosition = (Math.ceil(setLeftPosition / 100)) * 100;
-                    slideimgReel();
+          if (startX > lastX && setLeftPosition < ((imgArray.length-1) * 100)) {                              //if left swipe direction and not last slide
+                    setLeftPosition = (Math.ceil(setLeftPosition / 100)) * 100;                               //ceil setLeftPosition
+                    slideimgReel();                                                                           
                     imgArrayIndex++;
                     updatePagination();
                     addRemoveNextPreviousButtons();
